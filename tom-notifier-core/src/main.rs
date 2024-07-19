b0a678ca9b8868fb1fa58fcafde07bba44f81618
OverworkedCriminal <tom.swirgon@gmail.com>
2024-07-19 19:29:43 +0200

    load env variables from .env file

--- tom-notifier-core/src/main.rs
+++ tom-notifier-core/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    
+    #[cfg(debug_assertions)]
+    {
+        // Ignore error because .env file is not required
+        // as long as env variables are set
+        let _ = dotenvy::dotenv();
+    }
 }
