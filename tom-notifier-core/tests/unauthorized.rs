266fd7a81804d7d05823ee37b8f0954749c7eff5
OverworkedCriminal <tom.swirgon@gmail.com>
2024-07-21 18:39:07 +0200

    unauthorized e2e tests

---
+++ tom-notifier-core/tests/unauthorized.rs
@@ -0,0 +1,141 @@
+//!
+//! Module testing if all paths are protected by auth middleware.
+//!
+//! Any request should return 401 if URI and method is correct, 404 otherwise
+//!
+
+use bson::oid::ObjectId;
+use reqwest::{Client, StatusCode};
+
+fn address() -> String {
+    std::env::var("TOM_NOTIFIER_CORE_BIND_ADDRESS").unwrap()
+}
+
+#[tokio::test]
+async fn post_notifications_undelivered() {
+    let client = Client::new();
+
+    let response = client
+        .post(format!(
+            "http://{}/api/v1/notifications/undelivered",
+            address()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn get_notifications_undelivered() {
+    let client = Client::new();
+
+    let response = client
+        .get(format!(
+            "http://{}/api/v1/notifications/undelivered",
+            address()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn put_notifications_undelivered_invalidate_at() {
+    let client = Client::new();
+
+    let response = client
+        .put(format!(
+            "http://{}/api/v1/notifications/undelivered/{}/invalidate_at",
+            address(),
+            ObjectId::new().to_hex()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn get_notifications_delivered() {
+    let client = Client::new();
+
+    let response = client
+        .get(format!(
+            "http://{}/api/v1/notifications/delivered",
+            address()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn get_notification_delivered() {
+    let client = Client::new();
+
+    let response = client
+        .get(format!(
+            "http://{}/api/v1/notifications/delivered/{}",
+            address(),
+            ObjectId::new().to_hex()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn delete_notification_delivered() {
+    let client = Client::new();
+
+    let response = client
+        .delete(format!(
+            "http://{}/api/v1/notifications/delivered/{}",
+            address(),
+            ObjectId::new().to_hex()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn put_notification_delivered_seen() {
+    let client = Client::new();
+
+    let response = client
+        .put(format!(
+            "http://{}/api/v1/notifications/delivered/{}/seen",
+            address(),
+            ObjectId::new().to_hex()
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn get_non_existent_uri() {
+    let client = Client::new();
+
+    let response = client
+        .get(format!("http://{}/this-uri-does-not-exist", address(),))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+}
