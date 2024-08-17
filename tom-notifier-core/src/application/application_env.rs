efef4f1a61d51452b743c03bbd5ede0fdf3d9a63
OverworkedCriminal <tom.swirgon@gmail.com>
2024-08-17 12:58:11 +0200

    update tom-notifier-core so it uses jwt_auth util functions

--- tom-notifier-core/src/application/application_env.rs
+++ tom-notifier-core/src/application/application_env.rs
@@ -1,6 +1,7 @@
 use anyhow::anyhow;
 use jsonwebtoken::{Algorithm, DecodingKey};
-use std::{net::SocketAddr, str::FromStr, time::Duration};
+use jwt_auth::util::{parse_jwt_algorithms, parse_jwt_key};
+use std::{net::SocketAddr, time::Duration};
 
 pub struct ApplicationEnv {
     pub log_directory: String,
@@ -37,12 +38,11 @@ impl ApplicationEnv {
         let max_http_content_len =
             Self::env_var("TOM_NOTIFIER_CORE_MAX_HTTP_CONTENT_LEN")?.parse()?;
         let jwt_algorithms =
-            Self::parse_jwt_algorithms(Self::env_var("TOM_NOTIFIER_CORE_JWT_ALGORITHMS")?)?;
+            parse_jwt_algorithms(Self::env_var("TOM_NOTIFIER_CORE_JWT_ALGORITHMS")?)?;
         let jwt_algorithm = jwt_algorithms.first().ok_or(anyhow!(
             "TOM_NOTIFIER_CORE_JWT_ALGORITHMS need to contain at least one algorithm"
         ))?;
-        let jwt_key =
-            Self::parse_jwt_key(jwt_algorithm, Self::env_var("TOM_NOTIFIER_CORE_JWT_KEY")?)?;
+        let jwt_key = parse_jwt_key(jwt_algorithm, Self::env_var("TOM_NOTIFIER_CORE_JWT_KEY")?)?;
         let rabbitmq_connection_string =
             Self::env_var("TOM_NOTIFIER_CORE_RABBITMQ_CONNECTION_STRING")?;
         let rabbitmq_notifications_exchange_name =
@@ -75,42 +75,5 @@ impl ApplicationEnv {
 
     fn env_var(name: &'static str) -> anyhow::Result<String> {
         std::env::var(name).map_err(|_| anyhow!("environment variable {name} not set"))
-    }
-
-    fn parse_jwt_algorithms(jwt_algorithms: String) -> Result<Vec<Algorithm>, anyhow::Error> {
-        let mut algorithms = Vec::new();
-
-        for algorithm_str in jwt_algorithms.split(',') {
-            let algorithm = Algorithm::from_str(algorithm_str)?;
-            algorithms.push(algorithm);
-        }
-
-        match algorithms.is_empty() {
-            true => Err(anyhow!("JWT_ALGORITHMS does not contain any algorithm")),
-            false => Ok(algorithms),
-        }
-    }
-
-    fn parse_jwt_key(
-        jwt_algorithm: &Algorithm,
-        jwt_key: String,
-    ) -> Result<DecodingKey, anyhow::Error> {
-        let jwt_key_bytes = jwt_key.as_bytes();
-
-        let key = match jwt_algorithm {
-            Algorithm::HS256 | Algorithm::HS384 | Algorithm::HS512 => {
-                DecodingKey::from_secret(jwt_key_bytes)
-            }
-            Algorithm::ES256 | Algorithm::ES384 => DecodingKey::from_ec_pem(jwt_key_bytes)?,
-            Algorithm::RS256 | Algorithm::RS384 | Algorithm::RS512 => {
-                DecodingKey::from_rsa_pem(jwt_key_bytes)?
-            }
-            Algorithm::PS256 | Algorithm::PS384 | Algorithm::PS512 => {
-                DecodingKey::from_rsa_pem(jwt_key_bytes)?
-            }
-            Algorithm::EdDSA => DecodingKey::from_ec_pem(jwt_key_bytes)?,
-        };
-
-        Ok(key)
     }
 }
