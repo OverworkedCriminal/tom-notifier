7e40f58949791eb029abae7eb357eb6b04273c92
OverworkedCriminal <tom.swirgon@gmail.com>
2024-08-18 10:12:27 +0200

    added tom_notifier_admin role

--- tom-notifier-ws-delivery/src/error.rs
+++ tom-notifier-ws-delivery/src/error.rs
@@ -2,14 +2,21 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use jwt_auth::error::MissingRoleError;
 
 #[derive(Debug, thiserror::Error)]
-pub enum Error {}
+pub enum Error {
+    #[error("auth error: {0}")]
+    Auth(#[from] MissingRoleError),
+}
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         tracing::warn!(err = %self);
 
-        StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        match self {
+            Error::Auth(_) => StatusCode::FORBIDDEN,
+        }
+        .into_response()
     }
 }
--- tom-notifier-ws-delivery/src/routing.rs
+++ tom-notifier-ws-delivery/src/routing.rs
@@ -1,5 +1,6 @@
 use crate::{
     application::{ApplicationMiddleware, ApplicationState},
+    auth::Role,
     dto::{input, output},
     error::Error,
 };
@@ -8,8 +9,9 @@ use axum::{
     http::StatusCode,
     response::Response,
     routing::{delete, get},
-    Json, Router,
+    Extension, Json, Router,
 };
+use jwt_auth::{functions::require_all_roles, User};
 use uuid::Uuid;
 
 pub fn routing(application_middleware: &ApplicationMiddleware) -> Router<ApplicationState> {
@@ -20,11 +22,17 @@ pub fn routing(application_middleware: &ApplicationMiddleware) -> Router<Applica
         .route("/ws/v1", get(websocket_upgrade))
 }
 
-async fn get_ticket() -> Result<(StatusCode, Json<output::WebSocketTicket>), Error> {
+async fn get_ticket(
+    Extension(user): Extension<User>,
+) -> Result<(StatusCode, Json<output::WebSocketTicket>), Error> {
     todo!("not implemented")
 }
 
-async fn delete_connection(Path(user_id): Path<Uuid>) -> Result<StatusCode, Error> {
+async fn delete_connection(
+    Extension(user): Extension<User>,
+    Path(user_id): Path<Uuid>,
+) -> Result<StatusCode, Error> {
+    require_all_roles(&user, &[Role::Admin.as_ref()])?;
     todo!("not implemented")
 }
 
@@ -33,4 +41,63 @@ async fn websocket_upgrade(
     ws: WebSocketUpgrade,
 ) -> Result<Response, Error> {
     todo!("not implemented")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::application::{self, ApplicationEnv};
+    use axum::{body::Body, http::Request};
+    use http::{header::AUTHORIZATION, Method};
+    use jwt_auth::test::create_jwt;
+    use std::sync::Once;
+    use tower::ServiceExt;
+
+    static BEFORE_ALL: Once = Once::new();
+
+    fn init_env_variables() {
+        let _ = dotenvy::dotenv();
+    }
+
+    fn mock_application_state() -> ApplicationState {
+        ApplicationState {}
+    }
+
+    fn routing() -> Router<ApplicationState> {
+        let env = ApplicationEnv::parse().unwrap();
+        let middleware = application::create_middleware(&env);
+
+        super::routing(&middleware)
+    }
+
+    fn create_user_bearer() -> String {
+        let jwt_algorithms = std::env::var("TOM_NOTIFIER_WS_DELIVERY_JWT_ALGORITHMS").unwrap();
+        let jwt_key = std::env::var("TOM_NOTIFIER_WS_DELIVERY_JWT_TEST_ENCODE_KEY").unwrap();
+
+        let jwt = create_jwt(Uuid::new_v4(), &[], jwt_algorithms, jwt_key);
+
+        format!("Bearer {jwt}")
+    }
+
+    #[tokio::test]
+    async fn delete_connection_403() {
+        BEFORE_ALL.call_once(init_env_variables);
+
+        let application_state = mock_application_state();
+
+        let response = routing()
+            .with_state(application_state)
+            .oneshot(
+                Request::builder()
+                    .method(Method::DELETE)
+                    .uri(format!("/api/v1/connection/{}", Uuid::new_v4()))
+                    .header(AUTHORIZATION, create_user_bearer())
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+    }
 }
