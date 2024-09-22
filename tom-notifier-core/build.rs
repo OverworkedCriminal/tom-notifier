0b0b4d47f0572387223c0df345370d4d81fe3f17
OverworkedCriminal <tom.swirgon@gmail.com>
2024-09-22 08:23:46 +0200

    core Dockerfile

--- tom-notifier-core/build.rs
+++ tom-notifier-core/build.rs
@@ -2,16 +2,22 @@
 //! Generate Protobuf code
 //!
 
+use std::path::PathBuf;
+
 fn main() {
+    let protobuf_path = ["..", "shared", "protobuf"]
+        .into_iter()
+        .collect::<PathBuf>();
+
     prost_build::Config::new()
         .include_file("protobuf.rs")
         .compile_protos(
             &[
-                "../shared/protobuf/notification.proto",
-                "../shared/protobuf/rabbitmq_confirmation.proto",
-                "../shared/protobuf/rabbitmq_notification.proto",
+                protobuf_path.join("notification.proto"),
+                protobuf_path.join("rabbitmq_confirmation.proto"),
+                protobuf_path.join("rabbitmq_notification.proto"),
             ],
-            &["../shared/protobuf/"],
+            &[protobuf_path],
         )
         .unwrap();
 }
